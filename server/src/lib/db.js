0f7838498cee287f986e53e747d529d60215450a
utkarshanand1023 <utkarshanand634@gmail.com>
2026-08-05 00:57:54 +0530

    mongodb_uri added

--- server/src/lib/db.js
+++ server/src/lib/db.js
@@ -1,12 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export async function connectDB() {
-  const uri = process.env.MONGODB_URI;
-  if (!uri) throw new Error('MONGODB_URI is not set');
-
-  mongoose.set('strictQuery', true);
-
-  await mongoose.connect(uri);
-  console.log('MongoDB connected');
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err);
+    process.exit(1);
+  }
 }
-
